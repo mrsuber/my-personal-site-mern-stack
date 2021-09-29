e841beb6b4e1e738c63cbc5f4d7e7f2eaa393754
mohamad <msiysinyuy@gmail.com>
2021-09-29 23:09:52 +0100

    the authentication front end and back end done

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # my-personal-site-mern-stack
+1.)client site(react app)
+2.)backend capable of managing users
+3.)landing page and login pages
+4.)email reset password implemented.
---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import {BrowserRouter as Router,Switch,Route } from 'react-router-dom'
+
+//Routing
+import {PrivateRoute} from "./components"
+// auth screens
+import {PrivateScreen,LoginScreen,RegisterScreen,ForgotPasswordScreen,ResetPasswordScreen} from "./components"
+
+
+const App=()=> {
+  return (
+    <Router>
+      <div className='App'>
+        <Switch>
+            <PrivateRoute exact path="/" component={PrivateScreen}/>
+            <PrivateRoute exact path="/admin" component={PrivateScreen}/>
+            <Route exact path="/login" component={LoginScreen}/>
+            <Route exact path="/register" component={RegisterScreen}/>
+            <Route exact path="/forgotpassword" component={ForgotPasswordScreen}/>
+            <Route exact path="/passwordreset/:resetToken" component={ResetPasswordScreen}/>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
